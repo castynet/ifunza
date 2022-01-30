fd909a1d16824ef7ce592927f23358e57591da7a
castynet <riungemaina@gmail.com>
2022-01-30 17:28:12 +0300

    index: firebase init app

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import "antd/dist/antd.css";
+import { BrowserRouter } from "react-router-dom";
+import { initFirebaseApp } from "./firebase";
+
+initFirebaseApp();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
