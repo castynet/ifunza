5f55ef38b4cd3694934a93eed444377214e1e436
castynet <riungemaina@gmail.com>
2022-01-30 17:27:45 +0300

    App: added routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+import { useStore } from "./store";
+
+import StepOne from "components/stepOne/stepOne";
+import StepTwo from "components/stepTwo/stepTwo";
+import StepThree from "components/stepThree/stepThree";
+import WhereAmI from "components/whereAmI/whereAmI";
+
 export default function App() {
+  const whereAmI = useStore((state) => state.whereAmI);
+  console.log(whereAmI);
+
   return (
     <>
-      <div data-testid="app"></div>
+      <div data-testid="app" id="app">
+        <WhereAmI />
+        <Routes>
+          <Route path="/" element={<StepOne />} />
+          <Route path="student" element={<StepOne />} />
+          <Route path="parent" element={<StepTwo />} />
+          <Route path="overview" element={<StepThree />} />
+        </Routes>
+      </div>
     </>
   );
 }
