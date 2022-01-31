012ee0f502c555efb65c506cfbca48753567088a
castynet <riungemaina@gmail.com>
2022-01-31 14:02:20 +0300

    store: updated update logic & default values

--- src/store.js
+++ src/store.js
@@ -1,23 +1,34 @@
 import create from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useStore = create((set) => ({
-  student: {
-    img: null,
-    dob: null,
-    aNo: null,
-    fName: null,
-    sName: null,
-  },
-  parent: {
-    img: null,
-    fName: null,
-    sName: null,
-    email: null,
-    phone: null,
-    address: null,
-  },
-  whereAmI: 0,
-  updateStudent: (data) => set((state) => ({ ...state, student: data })),
-  updateParent: (data) => set((state) => ({ ...state, parent: data })),
-  updateWhereAmI: (data) => set((state) => ({ ...state, whereAmI: data })),
-}));
+export const useStore = create(
+  persist(
+    (set) => ({
+      student: {
+        img: "",
+        dob: "",
+        aNo: "",
+        fName: "",
+        sName: "",
+      },
+      parent: {
+        img: "",
+        fName: "",
+        sName: null,
+        email: "",
+        phone: "",
+        address: "",
+      },
+      whereAmI: 0,
+      updateStudent: (data) =>
+        set((s) => ({ student: { ...s.student, ...data } })),
+      updateParent: (data) =>
+        set((s) => ({ parent: { ...s.parent, ...data } })),
+      updateWhereAmI: (data) => set((state) => ({ ...state, whereAmI: data })),
+    }),
+    {
+      name: "student & parent info",
+      getStorage: () => sessionStorage,
+    }
+  )
+);
