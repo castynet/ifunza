9b0f7e05dac9b46789249725569cbb477f92a150
castynet <riungemaina@gmail.com>
2022-01-31 14:01:20 +0300

    App: added route validation logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import { useStore } from "./store";
 
 import StepOne from "components/stepOne/stepOne";
@@ -7,8 +8,49 @@ import StepThree from "components/stepThree/stepThree";
 import WhereAmI from "components/whereAmI/whereAmI";
 
 export default function App() {
-  const whereAmI = useStore((state) => state.whereAmI);
-  console.log(whereAmI);
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+  const { student, parent, updateWhereAmI } = useStore((state) => state);
+
+  useEffect(() => {
+    // update the steps indicator using the url
+    if (pathname.includes("student")) updateWhereAmI(0);
+    if (pathname.includes("parent")) updateWhereAmI(1);
+
+    // if someone is already in the homepage let them be, do not redirect.
+    if (pathname === "/") {
+      updateWhereAmI(0);
+      return;
+    }
+
+    // if someone is anywhere else and has not filled in the required
+    // student details, redirect them to the student page
+    if (
+      student.sName === "" ||
+      student.fName === "" ||
+      student.dob === "" ||
+      student.aNo === ""
+    ) {
+      navigate("/student");
+      return;
+    }
+
+    // if someone is anywhere else and has not filled in the required
+    // parent details, redirect them to the parent page
+    if (
+      (parent.email === "" ||
+        parent.phone === "" ||
+        parent.fName === "" ||
+        parent.sName === "") &&
+      !pathname.includes("parent") &&
+      !pathname.includes("student")
+    )
+      navigate("/parent");
+
+    // if someone has filled in the required details & is on the review page
+    // update the steps indicator to 2
+    if (pathname.includes("review")) updateWhereAmI(2);
+  }, [pathname, navigate, student, parent, updateWhereAmI]);
 
   return (
     <>
@@ -18,7 +60,8 @@ export default function App() {
           <Route path="/" element={<StepOne />} />
           <Route path="student" element={<StepOne />} />
           <Route path="parent" element={<StepTwo />} />
-          <Route path="overview" element={<StepThree />} />
+          <Route path="review" element={<StepThree />} />
+          <Route path="*" element={<StepOne />} />
         </Routes>
       </div>
     </>
